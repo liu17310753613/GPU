4a7b5e3a8f79ce98637f8ee85969780f893e96f1
liusj <295166119@qq.com>
2022-01-10 13:13:14 +0800

    .

--- src/GPU.java
+++ src/GPU.java
@@ -14,4 +14,19 @@ public class GPU {
         File ImgFile=new File(Path);
         ImageIO.write(Img,"png",ImgFile);
     }
+
+    public BufferedImage TurnGray(BufferedImage Img){
+        int width=Img.getWidth();
+        int height=Img.getHeight();
+        BufferedImage GrayImage =new BufferedImage(width,height,BufferedImage.TYPE_BYTE_GRAY);
+        for(int i=0 ;i<width;i++){
+            for(int j=0;j<height;j++){
+                int RGB=Img.getRGB(i,j);
+                Color color=new Color(RGB);
+                int Gray=((color.getRed()*30)+(color.getGreen()*59)+(color.getBlue()*11)+50)/100;
+                GrayImage.setRGB(i,j,Gray);
+            }
+        }
+        return GrayImage;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws AWTException, IOException {
         GPU gpu=new GPU();
-        BufferedImage screen=gpu.GetScreen();
         String path="D:\\fuck.png";
+        BufferedImage screen=gpu.GetScreen();
         gpu.SaveImage(screen,path);
+        BufferedImage GrayImage=gpu.TurnGray(screen);
+        path="D:\\gray.png";
+        gpu.SaveImage(GrayImage,path);
     }
 }
